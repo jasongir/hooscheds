97aae07dec5474805c41c2ddfad3bfef7e316154
Jason <snujugroix@gmail.com>
2022-10-31 20:53:05 -0400

    added DB connection script, proof of concept API routes

---
+++ backend-utils/db.ts
@@ -0,0 +1,38 @@
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const con = mysql.createConnection({
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: "hooscheds",
+});
+
+export const executeQuery = async (
+	query: string,
+	args: (string | number)[],
+	failureMessage: string
+) => {
+	try {
+		const result = await new Promise((resolve, reject) => {
+			con.query(query, args, function (error, results, fields) {
+				if (error) reject(error);
+				if (results) resolve(results);
+				if (fields) resolve(fields);
+			});
+		});
+
+		return result;
+	} catch (err) {
+		console.error(
+			(err as mysql.MysqlError).code && (err as mysql.MysqlError).message
+				? `${failureMessage} ${(err as mysql.MysqlError).code}, ${
+						(err as mysql.MysqlError).message
+				  }`
+				: err
+		);
+	}
+	return {};
+};
