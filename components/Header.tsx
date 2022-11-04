869d573c94a05143391fe9933bd67ca2731b7717
joykim1005 <81274936+joykim1005@users.noreply.github.com>
2022-11-03 23:39:17 -0400

    update navbar (hardcode)

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,26 +2,26 @@ import Link from "next/link";
 import { Container, Nav, Navbar, Row } from "react-bootstrap";
 
 const NavItem = ({ href, text }: { href: string; text: string }) => (
-	<Nav.Item>
-		<Navbar.Text>
-			<Link href={href}>{text}</Link>
-		</Navbar.Text>
-	</Nav.Item>
+  <Nav.Item>
+    <Navbar.Text>
+      <Link href={href}>{text}</Link>
+    </Navbar.Text>
+  </Nav.Item>
 );
 
 export default function Header() {
-	return (
-		<Navbar expand="md">
-			<Container>
-				<Navbar.Brand>
-					<Link href="/">HOME</Link>
-				</Navbar.Brand>
-				<NavItem href="/profile" text="My Profile" />
-				<NavItem href="/schedule" text="My Schedule" />
-				<NavItem href="/courses" text="Courses" />
-				<NavItem href="/friends" text="My Friends" />
-				<NavItem href="/logout" text="Logout" />
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar expand="md">
+      <Container>
+        <Navbar.Brand>
+          <Link href="/">HOME</Link>
+        </Navbar.Brand>
+        <NavItem href="/profile" text="My Profile" />
+        <NavItem href="/schedule" text="My Schedule" />
+        <NavItem href="/courses" text="Courses" />
+        <NavItem href="/friends/jag8thv" text="My Friends" />
+        <NavItem href="/logout" text="Logout" />
+      </Container>
+    </Navbar>
+  );
 }
