3c0511ad20bf04c32a931ef2598ae78a2fa772b7
joykim1005 <81274936+joykim1005@users.noreply.github.com>
2022-11-30 07:20:48 -0500

    like

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { useQueryClient } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
-import FullCalendar, { DaySeriesModel } from '@fullcalendar/react'
-import interactionPlugin from '@fullcalendar/interaction'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import { LoggedInStudent, getSchedules, getTimings, daysToNums } from "utils/utils";
+import FullCalendar, { DaySeriesModel } from "@fullcalendar/react";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import {
+	LoggedInStudent,
+	getSchedules,
+	getTimings,
+	daysToNums,
+	like,
+	getLikes,
+} from "utils/utils";
 import { useQuery } from "@tanstack/react-query";
+// import Heart = require("react-heart");
+import dynamic from "next/dynamic";
+// import { Suspense } from "react";
+import { useRouter } from "next/router";
+
+const Heart = dynamic(() => import("react-heart"), {
+	ssr: false,
+});
 
 /* TODO: 
           2. Find all sections included in that schedule from section_schedule
@@ -13,38 +28,91 @@ import { useQuery } from "@tanstack/react-query";
           4. Convert the section information into a form displayable in a calendar
           5. Display the schedule on a calendar */
 
-export default function DisplaySchedule() {
-  const queryClient = useQueryClient();
-  const student = queryClient.getQueryData(["auth"]) as LoggedInStudent
-    const { data:schedules, error:schedulesError} = useQuery(["Schedules"], () => getSchedules(student.student_id));
-    const { data:courses, error:coursesError} = useQuery(["Courses"], () => getTimings(student.student_id));
-    return courses && schedules && (
-    <>
-      <div className="p-3 text-center bg-light">
-        <h1 className="mb-3">{student.first_name}'s Schedule</h1>
-        <h3>{schedules[0].name}</h3>
-        
-                <FullCalendar
-          plugins={[interactionPlugin, timeGridPlugin]}
-          initialView='timeGridWeek'
-          nowIndicator={true}
-          editable={true}
-          events = { courses.map(
-            (course) => 
-            {
-              
-              return {
-                title: course.course_id, 
-                start: new Date(), 
-                groupId: course.course_id, 
-                daysOfWeek: daysToNums(course.meeting_dates),
-                startTime: course.start_time,
-                endTime: course.end_time}
-            }
-          )
-          }
-        />
-      </div>
-    </>);
-}
+const DisplaySchedule = () => {
+	const queryClient = useQueryClient();
+	const student = queryClient.getQueryData(["auth"]) as LoggedInStudent;
+	const {
+		data: schedules,
+		isLoading: schedulesAreLoading,
+		error: schedulesError,
+	} = useQuery(["Schedules"], () => getSchedules(student.student_id));
+	const { data: courses, error: coursesError } = useQuery(["Courses"], () =>
+		getTimings(student.student_id)
+	);
+	const { data: userLike, error: errorLike } = useQuery(["Likes"], () =>
+		getLikes(student.student_id)
+	);
+	// console.log("userLike:", userLike);
 
+	const [active, setActive] = useState(false);
+	useEffect(() => {
+		if (schedules) {
+			// we check if the current user's likes include the current post
+			userLike?.body.forEach((like) => {
+				if (like.schedule_id === schedules[0].schedule_id)
+					setActive(true);
+				else setActive(false);
+			});
+		}
+	}, [schedules, userLike, like, setActive]);
+
+	const likeMutation = useMutation(like, {
+		onSuccess: (data) => {
+			console.log("like works:", data);
+			queryClient.invalidateQueries(["Likes", "Schedules"]);
+		},
+		onError: (err) => console.log(err),
+	});
+
+	const onClickHandler = async () => {
+		// const { student_id_1, student_id_2 } = formState;
+		if (schedulesAreLoading || !schedules || schedules.length === 0) return;
+		const data = likeMutation.mutate({
+			student_id: student.student_id,
+			schedule_id: schedules[0].schedule_id,
+			isLiking: !active,
+		});
+		setActive(!active);
+		// console.log("like is now", active, data);
+		// router.push(`/schedule/${encodeURIComponent(student.student_id)}`);
+	};
+
+	return (
+		courses &&
+		schedules && (
+			<>
+				<div className="p-3 text-center bg-light">
+					<h1 className="mb-3">{student.first_name}'s Schedule</h1>
+					<h3>{schedules[0].name}</h3>
+					<FullCalendar
+						plugins={[interactionPlugin, timeGridPlugin]}
+						initialView="timeGridWeek"
+						nowIndicator={true}
+						editable={true}
+						events={courses.map((course) => {
+							return {
+								title: course.course_id,
+								start: new Date(),
+								groupId: course.course_id,
+								daysOfWeek: daysToNums(course.meeting_dates),
+								startTime: course.start_time,
+								endTime: course.end_time,
+							};
+						})}
+					/>
+				</div>
+
+				{/* <Suspense fallback={`Loading...`}>
+          <DynamicHeader />
+        </Suspense> */}
+
+				<div style={{ width: "2rem" }}>
+					<Heart isActive={active} onClick={onClickHandler} />
+					<p>{schedules[0].num_likes}</p>
+				</div>
+			</>
+		)
+	);
+};
+
+export default DisplaySchedule;
--- utils/utils.ts
+++ utils/utils.ts
@@ -3,197 +3,238 @@ import internal from "stream";
 
 //signup
 export interface Student {
-  student_id: string;
-  password: string;
-  first_name: string;
-  last_name: string;
-  year: number;
-  num_friends: number;
-  primary_major: string;
+	student_id: string;
+	password: string;
+	first_name: string;
+	last_name: string;
+	year: number;
+	num_friends: number;
+	primary_major: string;
 }
 
 export interface LoggedInStudent {
-  student_id: string;
-  first_name: string;
-  last_name: string;
-  year: number;
-  num_friends: number;
-  primary_major: string;
+	student_id: string;
+	first_name: string;
+	last_name: string;
+	year: number;
+	num_friends: number;
+	primary_major: string;
 }
 
 export interface StudentLogin {
-  student_id: string;
-  password: string;
+	student_id: string;
+	password: string;
 }
 
 export interface StudentFollow {
-  student_id_1: string;
-  student_id_2: string;
+	student_id_1: string;
+	student_id_2: string;
 }
 
 export interface FindFriend {
-  student_id: string;
+	student_id: string;
 }
 
 // export interface SearchFriendResponse {
 //   student: string;
 // }
 
-export type SearchFriendResponse = {
-  student: string;
-} | {success: boolean}
+export type SearchFriendResponse =
+	| {
+			student: string;
+	  }
+	| { success: boolean };
+
+export interface likeResponse {
+	success: boolean;
+	student_id: string;
+}
+
+export interface likeSchedule {
+	student_id: string;
+	schedule_id: string;
+	isLiking: boolean;
+}
+
+export interface userLike {
+	student_id: string;
+}
+
+export interface getLikeResponse {
+	success: boolean;
+	body: { student_id: string; schedule_id: string }[];
+}
+
+export async function getLikes(student_id: string): Promise<getLikeResponse> {
+	const { data } = await axios.get("/api/like", {
+		params: { student_id },
+	});
+	return data as getLikeResponse;
+}
+
+export async function like(likeData: likeSchedule): Promise<likeResponse> {
+	try {
+		const { data } = await axios.post("/api/like", likeData);
+		return data as likeResponse;
+	} catch (error) {
+		console.error(error);
+		return {
+			success: false,
+			student_id: "",
+		};
+	}
+}
 
 export interface followResponse {
-  success: boolean;
-  student_id: string;
+	success: boolean;
+	student_id: string;
 }
 
 export interface FollowFriend {
-  student_id_1: string;
-  student_id_2: string;
+	student_id_1: string;
+	student_id_2: string;
 }
 
-export async function follow({student_id_1, student_id_2}: FollowFriend): Promise<followResponse> {
-  const { data } = await axios.post("/api/follow", {
-    student_id_1,
-    student_id_2,
-  });
-  return data as followResponse
+export async function follow({
+	student_id_1,
+	student_id_2,
+}: FollowFriend): Promise<followResponse> {
+	const { data } = await axios.post("/api/follow", {
+		student_id_1,
+		student_id_2,
+	});
+	return data as followResponse;
 }
 
 export interface UnfollowFriend {
-  student_id_1: string;
-  student_id_2: string;
+	student_id_1: string;
+	student_id_2: string;
 }
 
 export interface unfollowResponse {
-  success: boolean;
-  student_id: string;
+	success: boolean;
+	student_id: string;
 }
 
-export async function unfollow({student_id_1, student_id_2}: UnfollowFriend): Promise<unfollowResponse> {
-  const { data } = await axios.post("/api/unfollow", {
-    student_id_1,
-    student_id_2,
-  });
-  return data as unfollowResponse
+export async function unfollow({
+	student_id_1,
+	student_id_2,
+}: UnfollowFriend): Promise<unfollowResponse> {
+	const { data } = await axios.post("/api/unfollow", {
+		student_id_1,
+		student_id_2,
+	});
+	return data as unfollowResponse;
 }
 
 export async function searchFriend({
-  student_id,
+	student_id,
 }: FindFriend): Promise<SearchFriendResponse> {
-  try{
-    const { data } = await axios.post("/api/searchFriend", {
-      student_id,
-    });
-    console.log("searchFriend working" , data);
-    return data as SearchFriendResponse;
-  } catch(err){
-    console.log(err)
-    return {success: false}
-  }
-
+	try {
+		const { data } = await axios.post("/api/searchFriend", {
+			student_id,
+		});
+		console.log("searchFriend working", data);
+		return data as SearchFriendResponse;
+	} catch (err) {
+		console.log(err);
+		return { success: false };
+	}
 }
 
 export interface PostResponse {
-  success: boolean;
+	success: boolean;
 }
 
 export interface Schedule {
-  schedule_id: string;
-  student_id: string;
-  name: string;
-  sched_term: string;
-  privacy: string;
-  num_likes: number;
+	schedule_id: string;
+	student_id: string;
+	name: string;
+	sched_term: string;
+	privacy: string;
+	num_likes: number;
 }
 
 export interface Timings {
-  course_id: string;
-  section_id: string;
-  start_time: string;
-  end_time: string;
-  meeting_dates: string;
-  location: string;
-  availability: string;
+	course_id: string;
+	section_id: string;
+	start_time: string;
+	end_time: string;
+	meeting_dates: string;
+	location: string;
+	availability: string;
 }
 
 export async function postStudent({
-  student_id,
-  password,
-  first_name,
-  last_name,
-  year,
-  primary_major,
+	student_id,
+	password,
+	first_name,
+	last_name,
+	year,
+	primary_major,
 }: Student): Promise<PostResponse> {
-  const { data } = await axios.post("/api/signup", {
-    student_id,
-    password,
-    first_name,
-    last_name,
-    year,
-    primary_major,
-  });
-  return data as PostResponse;
+	const { data } = await axios.post("/api/signup", {
+		student_id,
+		password,
+		first_name,
+		last_name,
+		year,
+		primary_major,
+	});
+	return data as PostResponse;
 }
 
 export async function getSchedules(student_id: string): Promise<Schedule[]> {
-  const { data } = await axios.get("/api/schedules/" + student_id);
-  return data as Schedule[];
+	const { data } = await axios.get("/api/schedules/" + student_id);
+	return data as Schedule[];
 }
 
 export async function getTimings(student_id: string): Promise<Timings[]> {
-  const { data:schedule } = await axios.get("/api/schedules/" + student_id);
-  const { data } = await axios.get("/api/timings/" + schedule[0].schedule_id);
-  return data as Timings[];
+	const { data: schedule } = await axios.get("/api/schedules/" + student_id);
+	const { data } = await axios.get("/api/timings/" + schedule[0].schedule_id);
+	return data as Timings[];
 }
 
 export async function getFriends(student_id: string): Promise<Student[]> {
-  let sid = student_id;
-  console.log(sid);
-  const { data } = await axios.get("/api/friends/" + sid);
-  return data as Student[];
+	let sid = student_id;
+	console.log(sid);
+	const { data } = await axios.get("/api/friends/" + sid);
+	return data as Student[];
 }
 
 export interface LoginResponse {
-  token: string;
-  student: LoggedInStudent;
+	token: string;
+	student: LoggedInStudent;
 }
 
 export async function postStudentLogIn({
-  student_id,
-  password,
+	student_id,
+	password,
 }: StudentLogin): Promise<LoginResponse> {
-  const { data } = await axios.post("/api/login", {
-    student_id,
-    password,
-  });
-  console.log(data);
-  return data as LoginResponse;
+	const { data } = await axios.post("/api/login", {
+		student_id,
+		password,
+	});
+	console.log(data);
+	return data as LoginResponse;
 }
 
-export function daysToNums(days: String):String[]{
-  let numArray:String[] = []
-  for (let i = 0; i <= days.length-2; i+= 2){
-    const day = days.slice(i, i+2);
-    if(day === 'Mo'){
-      numArray.push('1')
-    }
-    else if (day === 'Tu'){
-      numArray.push('2')
-    }
-    else if (day === 'We'){
-      numArray.push('3')
-    }
-    else if (day === 'Th'){
-      numArray.push('4')
-    }
-    else if (day === 'Fr'){
-      numArray.push('5')
-    }
+export function daysToNums(days: String): String[] {
+	let numArray: String[] = [];
+	for (let i = 0; i <= days.length - 2; i += 2) {
+		const day = days.slice(i, i + 2);
+		if (day === "Mo") {
+			numArray.push("1");
+		} else if (day === "Tu") {
+			numArray.push("2");
+		} else if (day === "We") {
+			numArray.push("3");
+		} else if (day === "Th") {
+			numArray.push("4");
+		} else if (day === "Fr") {
+			numArray.push("5");
+		}
+	}
 
-  }
-  
-  return numArray
-  
+	return numArray;
 }
